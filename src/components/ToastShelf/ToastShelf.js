06249ea1efccb8c851eb26257b3897ae67a8254a
George Kadas <kadas.george@gmail.com>
2023-05-30 17:19:09 +0200

    removed comments

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -4,8 +4,6 @@ import Toast from "../Toast";
 import styles from "./ToastShelf.module.css";
 
 function ToastShelf({ toasts, handleDismiss }) {
-  //Probably I need to handle this in Toast Playground.
-
   return (
     <ol className={styles.wrapper}>
       {toasts.map((toast, index) => (
